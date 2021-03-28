f7cfd5afb99b90d30ebca790c0f2f875095a8eed
gladson9 <gladsondsouza77@gmail.com>
2021-03-28 23:03:08 +0530

    added functionality

--- ColorPicker/app.js
+++ ColorPicker/app.js
@@ -1,7 +1,47 @@
 const generateBtn = document.querySelector(".generate");
+const colorsContainer = document.querySelectorAll(".color-wrapper");
+const copyKey = document.querySelectorAll("img");
 
+let initialColors = [];
+// Event listeners
 generateBtn.addEventListener("click", generateColors);
 
-function generateColors() {
-  console.log("working");
+copyKey.forEach((key) => {
+  key.addEventListener("click", () => {
+    let hex = initialColors[key.getAttribute("data-copy")];
+    copyToClipboard(hex);
+  });
+});
+
+//Functions
+
+function copyToClipboard(hex) {
+  const el = document.createElement("textarea");
+  el.value = hex;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
 }
+
+function generateColorCode() {
+  const values = "0123456789ABCDEF";
+  let hexCode = "#";
+  for (let i = 0; i < 6; i++) {
+    hexCode += values[Math.floor(Math.random() * values.length)];
+  }
+  return hexCode;
+}
+
+function generateColors() {
+  initialColors = [];
+  colorsContainer.forEach((div) => {
+    let hexCode = generateColorCode();
+    let hexTextField = div.children[1].children[0].children[0];
+    div.children[0].style.background = hexCode;
+    hexTextField.textContent = hexCode;
+    initialColors.push(hexCode);
+  });
+}
+
+generateColors();
